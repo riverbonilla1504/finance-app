53a256922667e1b3964ef5e98a69a8237a3fd92a
river PC <river.bonilla@campusucc.edu.co>
2025-04-01 16:54:17 -0500

    hotfix 2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,11 @@
-export const currencyFormatter = (amount: any) => {
-    const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    });
-    return formatter.format(amount);
+export const currencyFormatter = (amount: number) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  return formatter.format(amount);
 }
 
 
